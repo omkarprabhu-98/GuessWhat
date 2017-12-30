c6021d8d62d3347c750a6e789678e403f75cc5e3
rahulsnkr <rahulsnkr@gmail.com>
2017-12-31 01:55:00 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/omkar/guesswhat/MainActivity.java
+++ app/src/main/java/com/example/omkar/guesswhat/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
